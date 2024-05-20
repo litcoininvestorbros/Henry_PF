f816971b26707b80ec3c833fc590fc3e1517837d
Felix <santanafelixseb@gmail.com>
2024-05-20 19:51:15 -0300

    config a caraga dataset_yelp

--- src/mage-ai/mageai/data_loaders/dataset_yelp.py
+++ src/mage-ai/mageai/data_loaders/dataset_yelp.py
@@ -7,7 +7,6 @@ if 'test' not in globals():
 from glob import glob
 import pickle
 import pandas as pd
-from utils import carga_data as cd
 
 
 @data_loader
@@ -23,9 +22,45 @@ def load_data_from_file(*args, **kwargs):
     """
     
     path_data = '/home/mage/data/raw_parquets'
-    dataset: dict = cd.cargar_dataset_yelp(path_data)
 
-    return dataset
+    # Carga y-business.parquet
+    df_business = pd.read_parquet(f'{path_data}/y-business.parquet')
+    # Carga y-checkin.parquet
+    df_checkin = pd.read_parquet(f'{path_data}/y-checkin.parquet')
+    # Carga y-tip.parquet
+    df_tip = pd.read_parquet(f'{path_data}/y-tip.parquet')
+
+    # Carga y-review.parquet
+    # Crear lista de archivos en /data/raw/y-review/
+    review_parquets = glob(f'{path_data}/y-review/*')
+    # Crear dataframe donde se unen los datos extraidos de parquet
+    df_review = pd.DataFrame()
+    # Iterar por cada parquet dentro de /data/raw/y-review/
+    for p in review_parquets:
+        # Leer parquet
+        df = pd.read_parquet(p)
+        # Unir a df_review
+        df_review = pd.concat([df_review, df], ignore_index=True)
+    
+    # Crear lista de archivos en /data/raw/y-user/
+    user_parquets = glob(f'{path_data}/y-user/*')
+    user_parquets
+    # Crear dataframe donde se unen los datos extraidos de parquet
+    df_user = pd.DataFrame()
+    # Iterar por cada parquet dentro de /data/raw/y-user/
+    for p in user_parquets:
+        # Leer parquet
+        df = pd.read_parquet(p)
+        # Unir a df_user
+        df_user = pd.concat([df_user, df], ignore_index=True)
+    
+    return {
+        'business': df_business,
+        'checkin': df_checkin,
+        'tip': df_tip,
+        'review': df_review,
+        'user': df_user
+    }
 
 
 @test
