48cd9c56197512911f83b91869a2cb457d04ca86
Felix <santanafelixseb@gmail.com>
2024-05-21 18:38:16 -0300

    reorg Mage

--- src/mage-ai/mageai/scratchpads/test_import_utils.py
+++
@@ -1,7 +0,0 @@
-"""
-NOTE: Scratchpad blocks are used only for experimentation and testing out code.
-The code written here will not be executed as part of the pipeline.
-"""
-import utils.carga_data as cd
-
-cd.cargar_dataset_yelp()
