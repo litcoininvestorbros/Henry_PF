b51657eeaf9a55231f080b4ad0dd85f70b6c835e
Felix <santanafelixseb@gmail.com>
2024-06-01 14:53:40 -0300

    archivo que initializa modelo Mistral

---
+++ machine_learning/init_LLM.py
@@ -0,0 +1,9 @@
+from ctransformers import AutoModelForCausalLM
+
+model_Q5 = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-OpenOrca-GGUF", 
+                                        model_file="mistral-7b-openorca.Q5_K_M.gguf",
+                                        model_type="mistral",
+                                        max_new_tokens=256,
+                                        reset=True,
+                                        gpu_layers=0
+                                        )
