319d269072e460bb6e6fb93cf3e48ff199bc7c19
Felix <santanafelixseb@gmail.com>
2024-06-06 15:51:52 -0300

    auctualiza codigo de modelos ML

--- machine_learning/init_LLM.py
+++ machine_learning/init_LLM.py
@@ -1,9 +1,16 @@
+"""Funcion que descarga y inicializa el modelo Mistral-7B-OpenOrca-GGUF.Q5_K_M
+"""
 from ctransformers import AutoModelForCausalLM
 
-model_Q5 = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-OpenOrca-GGUF", 
-                                        model_file="mistral-7b-openorca.Q5_K_M.gguf",
-                                        model_type="mistral",
-                                        max_new_tokens=256,
-                                        reset=True,
-                                        gpu_layers=0
-                                        )
+
+def main():    
+    model_Q5 = AutoModelForCausalLM.from_pretrained("TheBloke/Mistral-7B-OpenOrca-GGUF", 
+        model_file="mistral-7b-openorca.Q5_K_M.gguf",
+        model_type="mistral",
+        max_new_tokens=256,
+        reset=True,
+        gpu_layers=0
+    )
+
+if __name__ == '__main__':
+    main()
